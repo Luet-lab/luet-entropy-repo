0e47a27bb1bd0249c72ffe0c74cc8039bd39b991
Ettore Di Giacinto <mudler@gentoo.org>
2020-03-13 22:24:08 +0100

    Overwrite existing specs

--- scripts/convert_entities.sh
+++ scripts/convert_entities.sh
@@ -4,13 +4,13 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 for i in $(ls $ROOT_DIR/tree/sabayonlinux.org/acct-group);do 
 echo "Converting $i"
 
-bash -i $ROOT_DIR/scripts/generate-entities.sh acct-group/$i $ROOT_DIR/tree/sabayonlinux.org/acct-group/$i/1
+bash -i $ROOT_DIR/scripts/generate-entities.sh acct-group/$i $ROOT_DIR/tree/sabayonlinux.org/acct-group/$i/0
 
 done
 
 for i in $(ls $ROOT_DIR/tree/sabayonlinux.org/acct-user);do 
 echo "Converting $i"
 
-bash -i $ROOT_DIR/scripts/generate-entities.sh acct-user/$i $ROOT_DIR/tree/sabayonlinux.org/acct-user/$i/1
+bash -i $ROOT_DIR/scripts/generate-entities.sh acct-user/$i $ROOT_DIR/tree/sabayonlinux.org/acct-user/$i/0
 
 done
