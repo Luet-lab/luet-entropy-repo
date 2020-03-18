4434b77636d7b010c188c2257e0f01d90e337ad2
Daniele Rondina <geaaru@sabayonlinux.org>
2020-03-18 12:57:09 +0100

    layer/sabayon-base-9999: Use external pkglist with right entropy version

--- tree/layers/base/9999/installer.sh
+++ tree/layers/base/9999/installer.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
 LUET_ENTROPY_REPO=${LUET_ENTROPY_REPO:-https://github.com/Luet-lab/luet-entropy-repo.git}
-LUET_EXCLUDES="${LUET_EXCLUDES:---exclude acct-group --exclude acct-user --exclude}"
-LUET_ENTROPY_PATH=${LUET_ENTROPY_PATH:-tree/sabayonlinux.org}
 
-git clone ${LUET_ENTROPY_REPO} repo
-
-wget https://dispatcher.sabayon.org/sbi/namespace/luet-cross-bin/luet-latest-amd64 -O ./luet
-chmod a+x ./luet
-
-# Build packages list
-pkgs="$(./luet tree pkglist -t ./repo/${LUET_ENTROPY_PATH} ${LUET_EXCLUDES} -v)"
-n_pkgs="$(./luet tree pkglist -t ./repo/${LUET_ENTROPY_PATH} ${LUET_EXCLUDES} -v | wc -l)"
 equo update && equo upgrade
-equo i ${pkgs}
+n_pkgs=$(cat ./pkglist | wc -l)
+equo i `cat ./pkglist | xargs echo`
 
 equo cleanup
-rm -rf ./repo ./luet ./installer.sh
 
+rm ./installer.sh ./pkglist
 echo "Installed $n_pkgs!"
