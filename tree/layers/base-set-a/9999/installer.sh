32c2659f9696d58b811f83dae14b2ca746108b88
Daniele Rondina <geaaru@sabayonlinux.org>
2020-03-18 19:18:50 +0100

    [sabayonlinux.org] sabayon/base-set-a/installer.sh: Require ACCEPT_LICENSE

--- tree/layers/base-set-a/9999/installer.sh
+++ tree/layers/base-set-a/9999/installer.sh
@@ -2,6 +2,8 @@
 
 LUET_ENTROPY_REPO=${LUET_ENTROPY_REPO:-https://github.com/Luet-lab/luet-entropy-repo.git}
 
+export ACCEPT_LICENSE=*
+
 equo update && equo upgrade
 n_pkgs=$(cat ./pkglist | wc -l)
 equo i `cat ./pkglist | xargs echo`
