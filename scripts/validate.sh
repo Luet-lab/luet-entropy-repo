0ed865c996f4b78c8528fb8c39cdc24a34d24034
Ettore Di Giacinto <mudler@gentoo.org>
2020-04-25 17:09:35 +0200

    Add VALIDATE_OPTIONS to make validate

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-
+VALIDATE_OPTIONS="${VALIDATE_OPTIONS:-}"
 if [ ! -d "$ROOT_DIR/luet-repo" ]; then
   git clone https://github.com/Luet-lab/luet-repo $ROOT_DIR/luet-repo
 fi
 
 if [ -d "$ROOT_DIR/luet-repo" ]; then
-  $LUET tree validate --tree $ROOT_DIR/$TREE --tree $ROOT_DIR/luet-repo -s
+  $LUET tree validate --tree $ROOT_DIR/$TREE --tree $ROOT_DIR/luet-repo ${VALIDATE_OPTIONS}
 else
   echo "luet-repo missing: bailing out"
   exit 1
