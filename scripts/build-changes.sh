3b3ecc35f755364d2dcaa39b3491c898a83c2389
Ettore Di Giacinto <mudler@gentoo.org>
2020-05-01 16:12:51 +0200

    Add build-changes script

---
+++ scripts/build-changes.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Auto detect script by Ettore Di Giacinto <mudler@sabayonlinux.org>
+# License: MIT
+# This script does one thing: it compares commits to branch and returns the packages
+# that has been touched between the two hash
+
+COMMIT="${COMMIT:-}"
+FROM_BRANCH="${FROM_BRANCH:-master}"
+
+# Get the files modified from COMMIT to the base branch (FROM_BRANCH)
+file=$(git --no-pager diff --name-only "${COMMIT}" $(git merge-base "${COMMIT}" "${FROM_BRANCH}"))
+
+TO_PROCESS=()
+TO_REMOVE=()
+for f in $file; do
+
+    if [ -f "${f}" ]; then
+        #echo "$f exists or has been touched"
+        touched_file="$(basename ${f})"
+        if [[ "$touched_file" == "build.yaml" ]] || [[ "$touched_file" == "definition.yaml" ]]; then
+            TO_PROCESS+=( $(dirname ${f}) )
+            #echo "Process $(dirname ${f})"
+        fi
+    else
+        TO_REMOVE+=( $(dirname ${f}) )
+        # Todo: remove corresponding packages from the repo
+    fi
+
+
+done
+
+# Get uniques
+TO_PROCESS=($(echo "${TO_PROCESS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+TO_REMOVE=($(echo "${TO_REMOVE[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+for p in ${TO_PROCESS[@]}; do
+   
+    ORIGINAL_PACKAGE_NAME=$(yq r $p/definition.yaml 'name')
+    ORIGINAL_PACKAGE_CATEGORY=$(yq r $p/definition.yaml 'category')
+    ORIGINAL_PACKAGE_VERSION=$(yq r $p/definition.yaml 'version')
+    echo "$ORIGINAL_PACKAGE_CATEGORY/$ORIGINAL_PACKAGE_NAME-$ORIGINAL_PACKAGE_VERSION"
+done
